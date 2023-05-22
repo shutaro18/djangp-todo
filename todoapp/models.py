34cf9f2b9eb26fed001b3a8191b71923b910a05f
shu <cp.aigis18@gmail.com>
2023-05-22 11:39:51 +0900

    models.pyマイグレート完了

--- todoapp/models.py
+++ todoapp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Task(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    title = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
+    completed = models.BooleanField(default=False)
+    createdDate = models.DateTimeField(auto_now_add=True)
+
+    # __str__メソッドは、オブジェクトを文字列として表現するために使用されます。
+    # このメソッドは、モデルのインスタンスを文字列に変換するために使用されます。
+    def __str__(self):
+        return self.title
+
+    # Metaクラスはモデルの振る舞いを定義するためのものです。
+    # orderingは、モデルのクエリセット（データベースからのデータの取得）の結果を並べ替えるために使用されます。
+    class Meta:
+        ordering = ["completed"]
